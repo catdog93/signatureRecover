12505f5f8308e630c0dc4cd0314b5e01aa74ed0b
IvanPavlov <ip@everstake.one>
2020-12-30 14:22:27 +0200

    v3 hash

--- main/main.go
+++ main/main.go
@@ -6,12 +6,13 @@ import (
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/crypto"
 	signer "github.com/ethereum/go-ethereum/signer/core"
+	"log"
 )
 
 const (
-	Signature = "3c99d3de749ced00853bf47eb8e408f192310b03ab755cc5aa6456c5507baa88" +
-		"5bed78ebc1e802de48a9b927cefa01afbcdac74b65a35913cbb4c5bf3071a0ce" +
-		"1c"
+	Signature = "0xdf9b1e7ced2f7d2def2ed1fc7fd740d5bfb9bba99ad84252475f84b47ba480" +
+		"da42864504cd61981b19b2393c764115eb7af1d6d61426937f4fe9baa8f6a50c70" +
+		"1b"
 	Address = "0x73500c296d4863cdd01fb30c231db927d007f26c"
 )
 
@@ -25,16 +26,18 @@ var (
 			"EIP712Domain": []signer.Type{
 				{Name: "name", Type: "string"},
 				{Name: "chainId", Type: "uint256"},
+				{Name: "verifyingContract", Type: "address"},
 				{Name: "version", Type: "string"},
 				{Name: "salt", Type: "string"},
 			},
 		},
 		PrimaryType: "ContactInfo",
 		Domain: signer.TypedDataDomain{
-			Name:    "Everstake ETH2 Staker",
-			Version: "1",
-			Salt:    "0x1122334455667788990011223344556677889900112233445566778899001122",
-			ChainId: math.NewHexOrDecimal256(3),
+			Name:              "Everstake ETH2 Staker",
+			Version:           "1",
+			VerifyingContract: "0x4aefd9A9BF4d0F19CD217ddB6467F3ad1e0A21FC",
+			Salt:              "0x1122334455667788990011223344556677889900112233445566778899001122",
+			ChainId:           math.NewHexOrDecimal256(3),
 		},
 		Message: signer.TypedDataMessage{
 			"email":    "test@email.com",
@@ -50,29 +53,29 @@ func main() {
 	rawData := []byte(fmt.Sprintf("\x19\x01%s%s", string(domainSeparator), string(typedDataHash)))
 	challengeHash := crypto.Keccak256Hash(rawData)
 
-	fmt.Println(hex.EncodeToString(domainSeparator)) // 710d7037aaaea992212008b33f833af7babbbfa7da895be1f7b398a69cfe2f77
+	fmt.Println(hex.EncodeToString(domainSeparator)) // d886b5763ff830579c2de06a7b8ba98c3cb819202d5c6010d030227042f44603
 	fmt.Println(hex.EncodeToString(typedDataHash))   // 4ec1628d891f1a5d124d4e9c9a2adea6cb3f11bd23ae4bf49a6c92b58035b12f
 
-	fmt.Println(hex.EncodeToString(rawData)) // 1901710d7037aaaea992212008b33f833af7babbbfa7da895be1f7b398a69cfe2f774ec1628d891f1a5d124d4e9c9a2adea6cb3f11bd23ae4bf49a6c92b58035b12f
-	fmt.Println("19014784d97a8aa5c03e35b64de520dc7ace934747e82df4c83f427da3c3751af1a34ec1628d891f1a5d124d4e9c9a2adea6cb3f11bd23ae4bf49a6c92b58035b12f")
+	fmt.Println(hex.EncodeToString(rawData)) // 1901d886b5763ff830579c2de06a7b8ba98c3cb819202d5c6010d030227042f446034ec1628d891f1a5d124d4e9c9a2adea6cb3f11bd23ae4bf49a6c92b58035b12f
+	fmt.Println("1901237b71f9fd1f5397d70d9ce661789f677895efcf97cd66f0818786662959cd6e4ec1628d891f1a5d124d4e9c9a2adea6cb3f11bd23ae4bf49a6c92b58035b12f")
 
-	fmt.Println(challengeHash) // 0x6d29d8b0d6ece6369543f4ac6a96b0cc4c14c21f9895f8ccd47642a419522f11
-	fmt.Println("0x" + "5f8e30e1754bb3b1932caed72165313bea2b3e012d9f9eb948815714d63ff8e1")
-	//sigBytes, err := hex.DecodeString(Signature)
-	//if err != nil {
-	//	log.Print(err)
-	//}
-	//
-	//sigBytes[64] -= 27
-	//
-	//sigPublicKey, err := crypto.SigToPub(challengeHash.Bytes(), sigBytes)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//fmt.Println(sigPublicKey)
-	//
-	//sigAddress := crypto.PubkeyToAddress(*sigPublicKey)
-	//
-	//fmt.Println(sigAddress.String())
-	//fmt.Println("expected address:", Address)
+	fmt.Println(challengeHash) // 0x8f7d50ceb9b6b006a2f3d7a9293f2b02148f6f13731e8c922dfad3f6860e5682
+	fmt.Println("0xb71caec68287c3a6805e093e9d7866f2a4830c47d73fe3cacba5e6fda2116b6b")
+	sigBytes, err := hex.DecodeString(Signature)
+	if err != nil {
+		log.Println(err)
+	}
+
+	sigBytes[64] -= 27
+
+	sigPublicKey, err := crypto.SigToPub(challengeHash.Bytes(), sigBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(sigPublicKey)
+
+	sigAddress := crypto.PubkeyToAddress(*sigPublicKey)
+
+	fmt.Println(sigAddress.String())
+	fmt.Println("expected address:", Address)
 }
